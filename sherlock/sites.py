69aed47985fe10eb381fe20a8bcccf4573c55a7f
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-05-31 11:24:08 +0300

    Update sites.py

--- sherlock/sites.py
+++ sherlock/sites.py
@@ -10,7 +10,7 @@ import requests
 import sys
 
 
-class SiteInfion():
+claSiteInfion():
     def __init__(self, name, url_home, url_username_format, username_claimed,
                  username_unclaimed, information):
         """Create Site Information Object.
