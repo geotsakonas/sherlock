0a1ddb682d66099b7ccb6b0a6835dc20b73b6ab8
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-05-31 11:54:35 +0300

    Update sherlock.py

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -10,16 +10,7 @@ networks.
 import csv
 import os
 import platform
-import re
-import sys
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from time import monotonic
-
-import requests
-
-from requests_futures.sessions import FuturesSession
-from torrequest import TorRequest
-from result import QueryStatus
+import QueryStatus
 from result import QueryResult
 from notify import QueryNotifyPrint
 from sites  import SitesInformation
